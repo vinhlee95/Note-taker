f51e1a661dc0cc278455c39de700fab9f9cf8749
Vinh Le <=>
2019-03-29 13:18:20 +0200

    Configure graphql and amplify

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    title
+    content
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    title
+    content
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    title
+    content
+  }
+}
+`;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
